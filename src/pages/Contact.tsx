ad76218f52d0b87ab38c01dd4c1f44225f535d85
Nada <86.saturn@gmail.com>
2025-05-27 16:02:54 +0300

    Modify contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,18 +4,20 @@ function Contact() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1 style={{ marginBottom: '1rem' }}>Contact</h1>
-    
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSf7lwBOj7TQC-0Xa2d4sI4_jzv271RgdcyeMuW6U0Fqq5fBxA/viewform?embedded=true"
-        width="100%"
-        height="7000"
-        frameBorder="0"
-        marginHeight={0}
-        marginWidth={0}
-        title="Contact Form"
-      >
-        Loading…
-      </iframe>
+
+      <div style={{ width: '100%', height: '100vh', minHeight: '800px' }}>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSf7lwBOj7TQC-0Xa2d4sI4_jzv271RgdcyeMuW6U0Fqq5fBxA/viewform?embedded=true"
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',
+          }}
+          title="Contact Form"
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 }
